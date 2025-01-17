f966610d854db232980a9b275cf6e5f076542b67
Kasia Zawistowska <zawistowska.kasia@outlook.com>
2025-01-17 09:59:16 +0000

    feat(*) added an optional loading builder to replace the default spinning indicator

--- lib/src/annotation_widget.dart
+++ lib/src/annotation_widget.dart
@@ -100,6 +100,11 @@ class ImageAnnotation extends StatefulWidget {
     Widget paintBoundary,
   )? builder;
 
+  /// Optional custom loading builder when [imageSize] or [imageOffset] are null
+  ///
+  /// Defaults to a 45x45 [CircularProgressIndicator].
+  final Widget Function(BuildContext context)? loadingBuilder;
+
   const ImageAnnotation({
     super.key,
     required this.imagePath,
@@ -108,6 +113,7 @@ class ImageAnnotation extends StatefulWidget {
     this.onDrawStart,
     this.onDrawEnd,
     this.builder,
+    this.loadingBuilder,
     this.color,
     this.strokeWidth,
     this.fontSize,
@@ -139,6 +145,7 @@ class _ImageAnnotationState extends State<ImageAnnotation> {
   @override
   void initState() {
     super.initState();
+
     _controller = ImageAnnotationController(
       widget.annotationType,
       color: widget.color,
@@ -300,53 +307,58 @@ class _ImageAnnotationState extends State<ImageAnnotation> {
     });
 
     if (imageSize == null || imageOffset == null) {
-      // TODO: Implement an actual placeholder
-      return const CircularProgressIndicator();
+      return widget.loadingBuilder != null
+          ? widget.loadingBuilder!(context)
+          : const Center(
+              child: SizedBox(
+                height: 45,
+                width: 45,
+                child: CircularProgressIndicator(),
+              ),
+            );
     }
 
-    if (widget.builder != null) {
-      return widget.builder!(
-        context,
-        _controller,
-        ImageAnnotationPaintBoundary(
-          imageWidget: _imageWidget,
-          imageSize: imageSize!,
-          imageOffset: imageOffset!,
-          controller: _controller,
-          onDrawEnd: widget.onDrawEnd,
-          onDrawStart: widget.finalizeOnRelease
-              ? _handleDrawStartWithFinalize
-              : widget.onDrawStart,
-        ),
-      );
-    }
-
-    return GestureDetector(
-      onLongPress: _controller.clearAnnotations,
-      onDoubleTap: _controller.undoAnnotation,
-      onTapDown: (details) {
-        if (_controller.annotationType == AnnotationOption.text) {
-          _showTextAnnotationDialog(context, details.localPosition);
-        } else if (!widget.finalizeOnRelease) {
-          _controller.add(
-            ShapeAnnotation(
-              _controller.annotationType,
-              color: _controller.color,
-              strokeWidth: _controller.strokeWidth,
+    return widget.builder != null
+        ? widget.builder!(
+            context,
+            _controller,
+            ImageAnnotationPaintBoundary(
+              imageWidget: _imageWidget,
+              imageSize: imageSize!,
+              imageOffset: imageOffset!,
+              controller: _controller,
+              onDrawEnd: widget.onDrawEnd,
+              onDrawStart: widget.finalizeOnRelease
+                  ? _handleDrawStartWithFinalize
+                  : widget.onDrawStart,
+            ),
+          )
+        : GestureDetector(
+            onLongPress: _controller.clearAnnotations,
+            onDoubleTap: _controller.undoAnnotation,
+            onTapDown: (details) {
+              if (_controller.annotationType == AnnotationOption.text) {
+                _showTextAnnotationDialog(context, details.localPosition);
+              } else if (!widget.finalizeOnRelease) {
+                _controller.add(
+                  ShapeAnnotation(
+                    _controller.annotationType,
+                    color: _controller.color,
+                    strokeWidth: _controller.strokeWidth,
+                  ),
+                );
+              }
+            },
+            child: ImageAnnotationPaintBoundary(
+              imageWidget: _imageWidget,
+              imageSize: imageSize!,
+              imageOffset: imageOffset!,
+              controller: _controller,
+              onDrawEnd: widget.onDrawEnd,
+              onDrawStart: widget.finalizeOnRelease
+                  ? _handleDrawStartWithFinalize
+                  : widget.onDrawStart,
             ),
           );
-        }
-      },
-      child: ImageAnnotationPaintBoundary(
-        imageWidget: _imageWidget,
-        imageSize: imageSize!,
-        imageOffset: imageOffset!,
-        controller: _controller,
-        onDrawEnd: widget.onDrawEnd,
-        onDrawStart: widget.finalizeOnRelease
-            ? _handleDrawStartWithFinalize
-            : widget.onDrawStart,
-      ),
-    );
   }
 }
