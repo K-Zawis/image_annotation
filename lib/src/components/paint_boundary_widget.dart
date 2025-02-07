81242e87988033c99c868df8ea1c1b5cccc0196e
Kasia Zawistowska <zawistowska.kasia@outlook.com>
2025-02-07 13:02:44 +0000

    feat(*) prevents polygon typed annotations from firing in `onPanUpdate`

--- lib/src/components/paint_boundary_widget.dart
+++ lib/src/components/paint_boundary_widget.dart
@@ -22,7 +22,8 @@ class AnnotationPaintBoundary extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<AnnotationPaintBoundary> createState() => _AnnotationPaintBoundaryState();
+  State<AnnotationPaintBoundary> createState() =>
+      _AnnotationPaintBoundaryState();
 }
 
 class _AnnotationPaintBoundaryState extends State<AnnotationPaintBoundary> {
