242ef8046b6e68a6a80e841640be004780e6b5eb
Kasia Zawistowska <zawistowska.kasia@outlook.com>
2025-02-07 09:44:40 +0000

    feat(*) Added groundwork for polywork support.

--- lib/src/models/annotation_enums.module.dart
+++ lib/src/models/annotation_enums.module.dart
@@ -3,6 +3,7 @@
 /// This enum is used by the [ImageAnnotation] widget to determine the type of annotation 
 /// to be applied. The available options are:
 /// - [line] : Draws a line annotation.
+/// - [polyline] : Draws a polygon using corner points.
 /// - [rectangle] : Draws a rectangle annotation.
 /// - [oval] : Draws an oval annotation.
 /// - [text] : Allows the user to add a text annotation to the image.
