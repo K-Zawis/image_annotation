459322953a808653f9cafc2a902b53372a310ae4
Kasia Zawistowska <zawistowska.kasia@outlook.com>
2025-02-13 17:02:05 +0000

    chore(*) testing draggable confirm and cancel buttons

--- lib/src/components/paint_boundary_widget.dart
+++ lib/src/components/paint_boundary_widget.dart
@@ -228,8 +228,6 @@ class _AnnotationPaintBoundaryState extends State<AnnotationPaintBoundary> {
                 ? (_polygonContainsThreePoints() ? _completePolygon : null)
                 : _completePolyline,
             onCancel: _drawingPolygon ? _cancelPolygon : _cancelPolyline,
-            withinBounds: _isWithinBounds,
-            boundarySize: _boundaryKey.currentContext?.size,
           ),
         // Row(
         //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -254,15 +252,11 @@ class _AnnotationPaintBoundaryState extends State<AnnotationPaintBoundary> {
 class DraggableConfirmationButtons extends StatefulWidget {
   final VoidCallback? onConfirm;
   final VoidCallback? onCancel;
-  final Function withinBounds;
-  final Size? boundarySize;
 
   const DraggableConfirmationButtons({
     super.key,
     required this.onConfirm,
     required this.onCancel,
-    required this.withinBounds,
-    required this.boundarySize,
   });
 
   @override
@@ -281,29 +275,28 @@ class _DraggableConfirmationButtonsState
       top: position.dy,
       child: GestureDetector(
         onPanUpdate: (details) {
-          if (widget.boundarySize == null) return;
-          if (widget.withinBounds
-              .call(details.localPosition, widget.boundarySize)) {
-            setState(() {
-              position += details.delta;
-            });
-          }
+          setState(() {
+            position += details.delta; 
+          });
         },
-        child: Row(
-          children: [
-            FloatingActionButton(
-              onPressed: widget.onConfirm,
-              backgroundColor: Colors.green,
-              child: const Icon(Icons.check),
-            ),
-            const SizedBox(width: 10),
-            FloatingActionButton(
-              onPressed: widget.onCancel,
-              backgroundColor: Colors.red,
-              child: const Icon(Icons.close),
-            ),
-          ],
+        child: Container(
+
         ),
+        // Row(
+        //   children: [
+        //     FloatingActionButton(
+        //       onPressed: widget.onConfirm,
+        //       backgroundColor: Colors.green,
+        //       child: const Icon(Icons.check),
+        //     ),
+        //     const SizedBox(width: 10),
+        //     FloatingActionButton(
+        //       onPressed: widget.onCancel,
+        //       backgroundColor: Colors.red,
+        //       child: const Icon(Icons.close),
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
