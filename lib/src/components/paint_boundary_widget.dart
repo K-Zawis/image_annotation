02078ad37184ba1c232792a2715ff668316082c5
Kasia Zawistowska <zawistowska.kasia@outlook.com>
2025-03-14 12:04:26 +0000

    chore(paint_boundary_widget.dart) testing behaviour

--- lib/src/components/paint_boundary_widget.dart
+++ lib/src/components/paint_boundary_widget.dart
@@ -210,7 +210,7 @@ class _AnnotationPaintBoundaryState extends State<AnnotationPaintBoundary> {
             onDraggableCanceled: (_, __) =>
                 setState(() => _movingPoint = false),
             onDragUpdate: (details) {
-              final clampedPosition = (details.localPosition).clamp(size);
+              final clampedPosition = (position - details.localPosition).clamp(size);
               final normalizedPosition = clampedPosition.toNormalized(size);
 
               final annotation =
