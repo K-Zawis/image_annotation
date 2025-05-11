12744140c569c01d9ae1810accabd9a947b1c034
Kasia Zawistowska <zawistowska.kasia@outlook.com>
2025-05-11 14:25:01 +0100

    fix(*) removed a breaking import that was imported by the autocomplete...

--- lib/src/components/annotation_widget.dart
+++ lib/src/components/annotation_widget.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
